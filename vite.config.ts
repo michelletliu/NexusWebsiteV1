32506bbdd29dbdeab379887510e36e6c6dc36eed
michelletliu <michelletliu@g.ucla.edu>
2025-11-09 02:18:37 -0800

    Update line opacity in Frame24 component and set Vite base path to root for all environments

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base: process.env.NODE_ENV === 'production' ? '/NexusWebsiteV1/' : '/',
+  base: '/',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
